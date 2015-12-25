c24bfdb574e32892dfb454330aeb7548c3ad6789
Ted Tang <tedtang77@gmail.com>
2015-12-25 08:09:04 +0000

    Initial Setup Testing

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(__dirname + '/public'));
+
+
+var fs = require( 'fs' );
+var input = "demo.html";
+var buf = new Buffer( fs.readFileSync( input ) ); 
+var output = buf.toString( 'utf8' );
+
+app.get('/', function(request, response) {
+  /*response.send('Hello World 2!')*/   
+  response.send( output );
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+  /*console.log(output); */
+})
